f86b1f873312d10991c36348e0fbd865160c5fee
wssung <126217240+wssung@users.noreply.github.com>
2023-02-24 10:19:25 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+var forntCamera = false;
+var currentenStream;
+
+const
+cameraView = document.querySelector("#camera-view"),
+cameraDevice = document.querySelector("#camera-device"),
+photoDisplay = document.querySelector("#photo-display"),
+takePhotoButton = document.querySelector("#take-photo-button");
+forntCameraButton = document.querySelector("#front-camera-button");
+
+function cameraStart(){
+    if (typeof currentStream !=='undefined'){
+        currentenStream.getTracks().array.forEach(track => {
+            track.stop();            
+        }); 
+    }
+
+    var constraints = { video: { facingMode: (frontCamera? "user" : "environment") }, audio: false };
+
+    navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function(stream){
+        currentStream = stream;
+        cameraDevice.srcObject = stream;
+    })
+    .catch(function(error){
+        console.error("Error happened.", error);
+    })
+}
+
+takePhotoButton.onclick = function() {
+    cameraView.width = cameraDevice.videoWidth;
+    cameraView.height = cameraDevice.videoHeight;
+    cameraView.getContext("2d").drawImage(cameraDevice, 0, 0);
+    photoDisplay.src = cameraView.toDataURL("image/webp");
+    photoDisplay.classList.add("photo-taken");
+};
+
+frontCameraButton.onclick = function() {
+    frontCamera = !frontCamera;
+    if (frontCamera) {
+        frontCameraButton.textContent = "Back Camera";
+    }
+    else {
+        frontCameraButton.textContent = "Front Camera";
+    }
+    cameraStart();
+};
+
+window.addEventListener("load", cameraStart);
