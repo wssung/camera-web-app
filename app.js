bab2bb6a7a10693fc41ec76e53325b74d633343a
wssung <126217240+wssung@users.noreply.github.com>
2023-03-03 09:48:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var frontCamera = false;
 var currentStream;
 
-// Define constants
-// Get the element in the document with id="camera-view", "camera-device", "photo-display", "take-photo-button" and "front-camera-button"
 const
     cameraView = document.querySelector("#camera-view"),
     cameraDevice = document.querySelector("#camera-device"),
@@ -10,23 +8,14 @@ const
     takePhotoButton = document.querySelector("#take-photo-button");
     frontCameraButton = document.querySelector("#front-camera-button");
 
-// Access the device camera and stream to cameraDevice
 function cameraStart() {
-    // Stop the video streaming before access the media device
     if (typeof currentStream !== 'undefined') {
         currentStream.getTracks().forEach(track => {
             track.stop();
         });
     }
 
-// Set constraints for the video stream
-// If frontCamera is true, use front camera
-// Otherwise, user back camera
-// "user" => Front camera
-// "environment" => Back camera
     var constraints = { video: { facingMode: (frontCamera? "user" : "environment") }, audio: false };
-    
-    // Access the media device, camera in this example
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
@@ -38,7 +27,6 @@ function cameraStart() {
         });
 }
 
-// If takePhotoButton clicked => Take and display a photo
 takePhotoButton.onclick = function() {
     cameraView.width = cameraDevice.videoWidth;
     cameraView.height = cameraDevice.videoHeight;
@@ -47,23 +35,14 @@ takePhotoButton.onclick = function() {
     photoDisplay.classList.add("photo-taken");
 };
 
-// If Front/Back camera is click => Change to front/back camera accordingly
 frontCameraButton.onclick = function() {
-    // Toggle the frontCamera variable
     frontCamera = !frontCamera;
-    // Setup the button text
     if (frontCamera) {
         frontCameraButton.textContent = "Back Camera";
     }
     else {
         frontCameraButton.textContent = "Front Camera";
     }
-    // Start the video streaming
     cameraStart();
 };
-
-// Start the camera and video streaming when the window loads
-// 1st parameter: Event type
-// 2nd parameter: Function to be called when the event occurs
 window.addEventListener("load", cameraStart);
-
